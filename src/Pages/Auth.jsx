fcfa837b5e4235f9f5ac1dbeb3caa24568e956f1
ridwanade0 <royeniyiadegboyega@gmail.com>
2024-05-09 17:15:14 +0100

    updated login page

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,18 +1,20 @@
 // import Login from "../Components/Auth/Login";
-// import image from "../assets/Images/ecommerce_cart.jpeg";
+import image from "../assets/Images/ecommerce-image.png";
+import logo from "../assets/Images/online-shop-logo.png";
+import googleicon from "../assets/Images/googleicon.png";
 
 const Auth = () => {
   return (
-    <div className="h-screen p-4 flex flex-row justify-center items-center">
-      <div className="h-full p-2 bg-orange-500 text-white w-3/6 rounded-3xl md:hidden lg:block xl:block sm:hidden">
-        <div className="text-white text-5xl w-1/2 mx-auto">
-          <p>Simplify management with our dashboard.</p>
-        </div>
+    <div className="h-[95vh] p-4 flex flex-row justify-center items-center w-10/12 mx-auto mt-4 rounded-xl bg-white drop-shadow-2xl">
+      <div className="h-full ms-2">
+        <img
+          src={image}
+          alt="increase your sale with an ecommerce website"
+          className="h-full w-full rounded-3xl"
+        />
       </div>
-      <div className="h-full p-2 w-3/6 mt-4">
-        <h1 className="border border-black p-2 bg-white text-black font-semibold text-lg text-center w-32 mx-auto mb-3">
-          E-Commerce
-        </h1>
+      <div className="h-full p-2 w-3/6 mt-2 ms-8">
+        <img src={logo} alt="online shop" className="w-32 h-32 mx-auto" />
         <h1 className="text-black font-bold text-4xl text-center">
           Welcome back
         </h1>
@@ -23,17 +25,17 @@ const Auth = () => {
           <input
             type="email"
             placeholder="Email address"
-            className="p-4 bg-gray-200 border-0 w-full my-3 rounded-lg"
+            className="p-4 bg-gray-200 border-0 w-full my-3 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
           <input
             type="password"
             placeholder="Password"
-            className="p-4 bg-gray-200 border-0 w-full my-3 rounded-lg"
+            className="p-4 bg-gray-200 border-0 w-full my-3 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
-          <span className="hover:underline text-gray-300 float-end">
+          <span className="hover:underline text-gray-300 float-end pb-3">
             Forgot password?
           </span>
-          <button className="w-full p-3 text-white text-2xl rounded-lg bg-orange-500 my-2">
+          <button className="w-full p-3 text-white text-2xl rounded-xl bg-orange-500 my-2 hover:bg-orange-600 mt-3">
             Login
           </button>
         </form>
@@ -42,9 +44,14 @@ const Auth = () => {
           <span className="text-gray-300">or continue with</span>
           <hr className="w-1/4 border-t border-gray-300" />
         </div>
-        <div className="border border-gray-300 w-3/5 p-3 bg-white text-black text-xl font-medium my-3 text-center rounded-lg mx-auto">
+        {/* <div className="border border-gray-300 w-3/5 p-3 h-20 bg-white text-black text-xl font-medium my-3 text-center rounded-lg mx-auto hover:bg-gray-100 active:border-orange-500">
+          <img src={googleicon} alt="Google" className="" />
           Google
-        </div>
+        </div> */}
+        <button className="flex items-center justify-center w-3/5 h-16 p-3 bg-white text-black text-xl font-medium my-3 text-center rounded-xl mx-auto border border-gray-300 hover:bg-gray-100 active:border-orange-500">
+          <img src={googleicon} alt="Google" className="w-6 h-6 mr-2" />
+          Google
+        </button>
         <p className="text-gray-400 text-center mt-3">
           {"Don't have an account?"}{" "}
           <span className="text-orange-400 hover:underline">Signup</span>
